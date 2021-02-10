7dd8b3f631e880f4f0ad7370c1f5caeea10efca7
nantachai <noom555-5@hotmail.com>
2021-02-10 09:47:39 +0700

    1.5

--- tables.php
+++ tables.php
@@ -1,5 +1,14 @@
 <?php
     require_once("dbcon.php");
+    if(isset($_GET['delete'])){
+        echo $_GET['id'];
+        $sql = "DELETE FROM movies WHERE m_id = '{$_GET['id']}'";
+        if (mysqli_query($conn, $sql)) {
+          echo "Record deleted successfully";
+        } else {
+          echo "Error deleting record: " . mysqli_error($conn);
+        }
+      }
     $sql = "SELECT * FROM movies";
     if(isset($_GET['search_click'])) {
         $sql = "SELECT * FROM movies WHERE m_name LIKE '%{$_GET['search']}%'";
@@ -29,6 +38,9 @@
     <td><?php echo $row['m_name'];?></td>
     <td><?php echo $row['m_day'];?></td>
     <td><?php echo $row['m_time'];?></td>
+    <td align = "center">
+      <a href ="?delete=1&id=<?php echo $row['m_id']; ?>">ลบ</a>
+    </td>
   </tr>
   <?php 
   }
